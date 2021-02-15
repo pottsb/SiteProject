2047f7beb569bafc8fb282d9762c0e298704096d
pottsb <63508539+pottsb@users.noreply.github.com>
2021-02-15 23:30:42 +0000

    added content to about page

--- about.php
+++ about.php
@@ -25,11 +25,101 @@ $result = $conn->query($sql);
 		<div class="bodyContainer">
 			<?php include "includes/widgets.php" ?>
 			<div class="contentContainer">
-			<div class="createPostFormContainer">
-				<h2>About</h2>
+				<div class="createPostFormContainer">
+					<h2>About</h2>
+					<hr>
+					<p>Aliquam vel molestie eros. Integer ut mollis lectus, et fringilla justo. Fusce ornare accumsan ante, non tincidunt massa iaculis at. Suspendisse ultrices nulla ac vestibulum pellentesque. Ut blandit tempor tortor et porta. In lacinia mi lacus, posuere rutrum risus pharetra nec. Quisque nec convallis metus, sed vulputate enim. Nunc aliquet lobortis semper. Sed a semper urna. In nec elit porta, convallis risus vitae, lacinia felis. Aliquam placerat, nunc quis sollicitudin porttitor, erat lacus ornare ligula, nec congue orci risus id est. Sed mollis, enim ac dapibus tempus, neque ex facilisis elit, eu sagittis est mi at leo. Vestibulum lectus lacus, porttitor eu sodales eget, viverra et justo. Phasellus varius iaculis urna eu aliquam. Morbi porttitor laoreet magna, quis eleifend sem tincidunt in. Nunc quis mauris at turpis porttitor imperdiet eget vel purus.</p>
+					<h2>Who we are</h2>
+					<hr>
+					<div class="postColContainer">
+						<div class="postCollumn">
+							<div class="sociallink"><img src="<?php print($sitePath)?>images/profiles/avatar2.png" id="profilePicture"></div>
+							<h5>CEO</h5>
+							<p>Person 1</p>
+						</div>
+						<div class="postCollumn">
+						<div class="sociallink"><img src="<?php print($sitePath)?>images/profiles/avatar.png" id="profilePicture"></div>
+							<h5>CFO</h5>
+							<p>Person 2</p>
+						</div>
+						<div class="postCollumn">
+							<div class="sociallink"><img src="<?php print($sitePath)?>images/profiles/avatar2.png" id="profilePicture"></div>
+							<h5>CTO</h5>
+							<p>Person 3</p>
+						</div>
+				</div>
+				<div class="postColContainer">
+						<div class="postCollumn">
+							<div class="sociallink"><img src="<?php print($sitePath)?>images/profiles/avatar.png" id="profilePicture"></div>
+							<h5>Technical Manager</h5>
+							<p>Person 4</p>
+						</div>
+						<div class="postCollumn">
+						<div class="sociallink"><img src="<?php print($sitePath)?>images/profiles/avatar2.png" id="profilePicture"></div>
+							<h5>Head of Sales</h5>
+							<p>Person 5</p>
+						</div>
+						<div class="postCollumn">
+							<div class="sociallink"><img src="<?php print($sitePath)?>images/profiles/avatar.png" id="profilePicture"></div>
+							<h5>Customer Relantions</h5>
+							<p>Person 6</p>
+						</div>
+				</div>
+				<h2>Our Values</h2>
 				<hr>
-			Aliquam vel molestie eros. Integer ut mollis lectus, et fringilla justo. Fusce ornare accumsan ante, non tincidunt massa iaculis at. Suspendisse ultrices nulla ac vestibulum pellentesque. Ut blandit tempor tortor et porta. In lacinia mi lacus, posuere rutrum risus pharetra nec. Quisque nec convallis metus, sed vulputate enim. Nunc aliquet lobortis semper. Sed a semper urna. In nec elit porta, convallis risus vitae, lacinia felis. Aliquam placerat, nunc quis sollicitudin porttitor, erat lacus ornare ligula, nec congue orci risus id est. Sed mollis, enim ac dapibus tempus, neque ex facilisis elit, eu sagittis est mi at leo. Vestibulum lectus lacus, porttitor eu sodales eget, viverra et justo. Phasellus varius iaculis urna eu aliquam. Morbi porttitor laoreet magna, quis eleifend sem tincidunt in. Nunc quis mauris at turpis porttitor imperdiet eget vel purus.
-</div>
+				<p>Aliquam vel molestie eros. Integer ut mollis lectus, et fringilla justo. Fusce ornare accumsan ante, non tincidunt massa iaculis at. Suspendisse ultrices nulla ac vestibulum pellentesque. Ut blandit tempor tortor et porta. In lacinia mi lacus, posuere rutrum risus pharetra nec. Quisque nec convallis metus, sed vulputate enim. Nunc aliquet lobortis semper. Sed a semper urna. In nec elit porta, convallis risus vitae, lacinia felis. Aliquam placerat, nunc quis sollicitudin porttitor, erat lacus ornare ligula, nec congue orci risus id est. Sed mollis, enim ac dapibus tempus, neque ex facilisis elit, eu sagittis est mi at leo. Vestibulum lectus lacus, porttitor eu sodales eget, viverra et justo. Phasellus varius iaculis urna eu aliquam. Morbi porttitor laoreet magna, quis eleifend sem tincidunt in. Nunc quis mauris at turpis porttitor imperdiet eget vel purus.</p>
+				<h2>Why Us?</h2>
+				<hr>
+				<p>Aliquam vel molestie eros. Integer ut mollis lectus, et fringilla justo. Fusce ornare accumsan ante, non tincidunt massa iaculis at. Suspendisse ultrices nulla ac vestibulum pellentesque. Ut blandit tempor tortor et porta. In lacinia mi lacus, posuere rutrum risus pharetra nec. Quisque nec convallis metus, sed vulputate enim. Nunc aliquet lobortis semper. Sed a semper urna. In nec elit porta, convallis risus vitae, lacinia felis. Aliquam placerat, nunc quis sollicitudin porttitor, erat lacus ornare ligula, nec congue orci risus id est. Sed mollis, enim ac dapibus tempus, neque ex facilisis elit, eu sagittis est mi at leo. Vestibulum lectus lacus, porttitor eu sodales eget, viverra et justo. Phasellus varius iaculis urna eu aliquam. Morbi porttitor laoreet magna, quis eleifend sem tincidunt in. Nunc quis mauris at turpis porttitor imperdiet eget vel purus.</p>
+				<h2>What our clients are saying</h2>
+				<hr>	
+				<div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="carousel" style="height:300px;">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner" style="text-align:center; padding-top: 10%">
+          <div class="carousel-item active">
+
+              <h5>Consonto Inc</h5>
+              <p>Not that good but they're cheap AF.</p>
+            
+          </div>
+          <div class="carousel-item">
+
+              <h5>Tech Solutions LLC</h5>
+              <p>We'd be out of bussiness without their inovative products.</p>
+			  
+
+          </div>
+          <div class="carousel-item">
+
+              <h5>Jon Eastoe</h5>
+              <p>So good. Very Impressive</p>
+
+          </div>
+		  <div class="carousel-item">
+
+		  <h5>eBay user</h5>
+	      <p>A+++++ great eBayer</p>
+
+	    	</div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+	  <p>Aliquam vel molestie eros. Integer ut mollis lectus, et fringilla justo. Fusce ornare accumsan ante, non tincidunt massa iaculis at. Suspendisse ultrices nulla ac vestibulum pellentesque. Ut blandit tempor tortor et porta. In lacinia mi lacus, posuere rutrum risus pharetra nec. Quisque nec convallis metus, sed vulputate enim. Nunc aliquet lobortis semper. Sed a semper urna. In nec elit porta, convallis risus vitae, lacinia felis. Aliquam placerat, nunc quis sollicitudin porttitor, erat lacus ornare ligula, nec congue orci risus id est. Sed mollis, enim ac dapibus tempus, neque ex facilisis elit, eu sagittis est mi at leo. Vestibulum lectus lacus, porttitor eu sodales eget, viverra et justo. Phasellus varius iaculis urna eu aliquam. Morbi porttitor laoreet magna, quis eleifend sem tincidunt in. Nunc quis mauris at turpis porttitor imperdiet eget vel purus.</p>
+				
+					
+				</div>
 			</div>
 		</div>
 		<?php include "includes/footer.php" ?>
