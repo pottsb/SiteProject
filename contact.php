430ff8253178aa380f015945389e089d9e277c01
pottsb <63508539+pottsb@users.noreply.github.com>
2021-02-15 01:59:58 +0000

    updated contact page and info

--- contact.php
+++ contact.php
@@ -27,14 +27,97 @@ $result = $conn->query($sql);
 		<div class="bodyContainer">
 			<?php include "includes/widgets.php" ?>
 			<div class="contentContainer">
-			<div class="createPostFormContainer">
-				<h2>Contact</h2>
+				<div class="createPostFormContainer">
+					<h2>Contact</h2>
+					<hr>
 				</div>
-				<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
-  <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=University%20of%20Derby,%20Kedleston%20Road,%20Derby,%20DE22%201GB+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0"
-    style="border:0" allowfullscreen></iframe>
+				<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 300px">
+ 	 				<iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=University%20of%20Derby,%20Kedleston%20Road,%20Derby,%20DE22%201GB+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<div class="postColContainer">
+					<div class="postCollumn">
+						<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/icons/mappin.png" height="35"></div>
+						<p>University of Derby<br>
+						Kedleston Rd<br>
+						Derby DE22 1GB</p>
 					</div>
-  
+					<div class="postCollumn">
+						<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/icons/phone.png" height="35"></div>
+						<p>01332 123456<br>
+						071234567</p>
+					</div>
+					<div class="postCollumn">
+						<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/icons/email.png" height="35"></div>
+						<p>hello@company.tld<br>
+						sales@company.tld</p>
+					</div>
+
+				</div>	
+				<form class="needs-validation" novalidate>
+				<div class="createPostFormContainer">
+					<h2>Drop us a message?</h2>
+					<hr>
+					Message:<br>
+					<textarea name="message" class="form-control" rows="10" required></textarea><br>
+					<div class="valid-feedback">
+					Looks good!
+					</div>
+					<div class="invalid-feedback">
+					Please enter your message.
+					</div>
+					
+					
+					<div class="createPostFormCol">
+						Name: <input type="text" class="form-control" id="name" name="name" required>
+						<div class="valid-feedback">
+						Looks good!
+						</div>
+						<div class="invalid-feedback">
+						You need to introduce yourself!
+						</div>
+					</div>
+					<div class="createPostFormCol">
+						Email: <input type="text" class="form-control" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+						<div class="valid-feedback">
+						Looks good!
+						</div>
+						<div class="invalid-feedback">
+      					  Please enter a valid email.
+    			 		  </div>
+					</div>
+					<div class="createPostFormCol">
+						<br>
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</div>
+					  </form>
+					  <script>
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function () {
+        'use strict'
+      
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+      
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+          .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+              console.log("1")
+              if (!form.checkValidity()) {
+                console.log("2")
+                event.preventDefault()
+                event.stopPropagation()
+              }
+      
+              form.classList.add('was-validated')
+            }, false)
+          })
+      })()
+      
+      
+          </script>
+				</div>
+					
 			</div>
 		</div>
 		<?php include "includes/footer.php" ?>
--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,17 @@
 <div class="footerContainer">
 <div class="footerCollumn">
-    
+<h3>Chat with us</h3>
+
+<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/socials/linkedin.png" height="35">&nbsp;<a href="#">linked In</a></div>
+<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/socials/twitter.png" height="35">&nbsp;<a href="#">Twitter</a></div>
+<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/socials/reddit.png" height="35">&nbsp;<a href="#">Reddit</a></div>
+
 </div>
 <div class="footerCollumn">
-    
+    <h3>Check us out</h3>
+<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/socials/facebook.png" height="35">&nbsp;<a href="#">Facebook</a></div>
+<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/socials/instagram.png" height="35">&nbsp;<a href="#">Instagram</a></div>
+<div class="sociallink"><img src="http://lamp-cli.svr0.local/SiteProject/images/socials/youtube.png" height="35">&nbsp;<a href="#">YouTube</a></div>
 </div>
 <div class="footerCollumn">
     <h3>Meta</h3>
