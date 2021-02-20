2575a3d4bfe0b1fb6d3c6d62f48d4931b398faa9
pottsb <63508539+pottsb@users.noreply.github.com>
2021-02-20 14:29:17 +0000

    Updated links

--- includes/footer.php
+++ includes/footer.php
@@ -2,16 +2,16 @@
 <div class="footerCollumn">
 <h3>Chat with us</h3>
 
-<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/linkedin.png" height="35">&nbsp;<a href="#">linkedIn</a></div>
-<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/twitter.png" height="35">&nbsp;<a href="#">Twitter</a></div>
-<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/reddit.png" height="35">&nbsp;<a href="#">Reddit</a></div>
+<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/linkedin.png" height="35">&nbsp;<a href="https://linkedin.com">linkedIn</a></div>
+<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/twitter.png" height="35">&nbsp;<a href="https://twitter.com">Twitter</a></div>
+<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/reddit.png" height="35">&nbsp;<a href="https://reddit.com">Reddit</a></div>
 
 </div>
 <div class="footerCollumn">
     <h3>Check us out</h3>
-<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/facebook.png" height="35">&nbsp;<a href="#">Facebook</a></div>
-<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/instagram.png" height="35">&nbsp;<a href="#">Instagram</a></div>
-<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/youtube.png" height="35">&nbsp;<a href="#">YouTube</a></div>
+<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/facebook.png" height="35">&nbsp;<a href="https://facebook.com">Facebook</a></div>
+<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/instagram.png" height="35">&nbsp;<a href="https://instagram.com">Instagram</a></div>
+<div class="sociallink"><img src="<?php print($sitePath)?>images/icons/youtube.png" height="35">&nbsp;<a href="https://youtube.com">YouTube</a></div>
 </div>
 <div class="footerCollumn">
     <h3>Meta</h3>
--- order.php
+++ order.php
@@ -10,7 +10,6 @@ $result = $conn->query($sql);
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
 	<link rel="stylesheet" href="libs/bootstrap-5.0.0-beta2-dist/css/bootstrap.min.css" >
 	<link rel="stylesheet" href="style/style.css">
